2d7ca559e93a1cc5cab0482f1b9b69254f8745a5
Your Name <you@example.com>
2019-10-28 17:18:58 +0700

    asdsa

--- buoi2/script.js
+++ buoi2/script.js
@@ -18,7 +18,12 @@ $(document).ready(function () {
             $("#click-angle2").removeClass("fa-angle-down");
             $("#click-angle3").addClass("fa-angle-up");
             $("#click-angle3").removeClass("fa-angle-down");
-
+            $("#click-angle4").addClass("fa-angle-up");
+            $("#click-angle4").removeClass("fa-angle-down");
+            $("#click-angle5").addClass("fa-angle-up");
+            $("#click-angle5").removeClass("fa-angle-down");
+            $("#click-angle6").addClass("fa-angle-up");
+            $("#click-angle6").removeClass("fa-angle-down");
         }
         // console.log($("#click-angle").attr("class").split(" ")[2]);
         // $("i").toggleClass("fas fa-angle-down angle-down");
@@ -47,6 +52,12 @@ $(document).ready(function () {
             $("#click-angle2").addClass("fa-angle-up");
             $("#click-angle3").removeClass("fa-angle-down");
             $("#click-angle3").addClass("fa-angle-up");
+            $("#click-angle4").addClass("fa-angle-up");
+            $("#click-angle4").removeClass("fa-angle-down");
+            $("#click-angle5").addClass("fa-angle-up");
+            $("#click-angle5").removeClass("fa-angle-down");
+            $("#click-angle6").addClass("fa-angle-up");
+            $("#click-angle6").removeClass("fa-angle-down");
         }
         // console.log($("#click-angle").attr("class").split(" ")[2]);
         // $("i").toggleClass("fas fa-angle-down angle-down");
@@ -70,6 +81,12 @@ $(document).ready(function () {
             $("#click-angle1").addClass("fa-angle-up");
             $("#click-angle3").removeClass("fa-angle-down");
             $("#click-angle3").addClass("fa-angle-up");
+            $("#click-angle4").addClass("fa-angle-up");
+            $("#click-angle4").removeClass("fa-angle-down");
+            $("#click-angle5").addClass("fa-angle-up");
+            $("#click-angle5").removeClass("fa-angle-down");
+            $("#click-angle6").addClass("fa-angle-up");
+            $("#click-angle6").removeClass("fa-angle-down");
         }
         // console.log($("#click-angle").attr("class").split(" ")[2]);
         // $("i").toggleClass("fas fa-angle-down angle-down");
@@ -93,6 +110,102 @@ $(document).ready(function () {
             $("#click-angle2").addClass("fa-angle-up");
             $("#click-angle1").removeClass("fa-angle-down");
             $("#click-angle1").addClass("fa-angle-up");
+            $("#click-angle4").addClass("fa-angle-up");
+            $("#click-angle4").removeClass("fa-angle-down");
+            $("#click-angle5").addClass("fa-angle-up");
+            $("#click-angle5").removeClass("fa-angle-down");
+            $("#click-angle6").addClass("fa-angle-up");
+            $("#click-angle6").removeClass("fa-angle-down");
+
+        }
+        // console.log($("#click-angle").attr("class").split(" ")[2]);
+        // $("i").toggleClass("fas fa-angle-down angle-down");
+        // $("i").toggleClass("fa-angle-down", true);
+    });
+
+});
+$(document).ready(function () {
+    $("#toggleClass5").click(function () {
+        const xxx = $("#click-angle4").attr("class").split(" ")[2]
+        if (xxx === "fa-angle-down") {
+            $("#click-angle4").removeClass("fa-angle-down");
+            $("#click-angle4").addClass("fa-angle-up");
+
+        } else {
+            $("#click-angle4").removeClass("fa-angle-up");
+            $("#click-angle4").addClass("fa-angle-down");
+            $("#click-angle").removeClass("fa-angle-down");
+            $("#click-angle").addClass("fa-angle-up");
+            $("#click-angle2").removeClass("fa-angle-down");
+            $("#click-angle2").addClass("fa-angle-up");
+            $("#click-angle1").removeClass("fa-angle-down");
+            $("#click-angle1").addClass("fa-angle-up");
+            $("#click-angle3").addClass("fa-angle-up");
+            $("#click-angle3").removeClass("fa-angle-down");
+            $("#click-angle5").addClass("fa-angle-up");
+            $("#click-angle5").removeClass("fa-angle-down");
+            $("#click-angle6").addClass("fa-angle-up");
+            $("#click-angle6").removeClass("fa-angle-down");
+
+        }
+        // console.log($("#click-angle").attr("class").split(" ")[2]);
+        // $("i").toggleClass("fas fa-angle-down angle-down");
+        // $("i").toggleClass("fa-angle-down", true);
+    });
+
+});
+$(document).ready(function () {
+    $("#toggleClass6").click(function () {
+        const xxx = $("#click-angle5").attr("class").split(" ")[2]
+        if (xxx === "fa-angle-down") {
+            $("#click-angle5").removeClass("fa-angle-down");
+            $("#click-angle5").addClass("fa-angle-up");
+
+        } else {
+            $("#click-angle5").removeClass("fa-angle-up");
+            $("#click-angle5").addClass("fa-angle-down");
+            $("#click-angle").removeClass("fa-angle-down");
+            $("#click-angle").addClass("fa-angle-up");
+            $("#click-angle2").removeClass("fa-angle-down");
+            $("#click-angle2").addClass("fa-angle-up");
+            $("#click-angle1").removeClass("fa-angle-down");
+            $("#click-angle1").addClass("fa-angle-up");
+            $("#click-angle4").addClass("fa-angle-up");
+            $("#click-angle4").removeClass("fa-angle-down");
+            $("#click-angle3").addClass("fa-angle-up");
+            $("#click-angle3").removeClass("fa-angle-down");
+            $("#click-angle6").addClass("fa-angle-up");
+            $("#click-angle6").removeClass("fa-angle-down");
+
+        }
+        // console.log($("#click-angle").attr("class").split(" ")[2]);
+        // $("i").toggleClass("fas fa-angle-down angle-down");
+        // $("i").toggleClass("fa-angle-down", true);
+    });
+
+});
+$(document).ready(function () {
+    $("#toggleClass7").click(function () {
+        const xxx = $("#click-angle6").attr("class").split(" ")[2]
+        if (xxx === "fa-angle-down") {
+            $("#click-angle6").removeClass("fa-angle-down");
+            $("#click-angle6").addClass("fa-angle-up");
+
+        } else {
+            $("#click-angle6").removeClass("fa-angle-up");
+            $("#click-angle6").addClass("fa-angle-down");
+            $("#click-angle").removeClass("fa-angle-down");
+            $("#click-angle").addClass("fa-angle-up");
+            $("#click-angle2").removeClass("fa-angle-down");
+            $("#click-angle2").addClass("fa-angle-up");
+            $("#click-angle1").removeClass("fa-angle-down");
+            $("#click-angle1").addClass("fa-angle-up");
+            $("#click-angle4").addClass("fa-angle-up");
+            $("#click-angle4").removeClass("fa-angle-down");
+            $("#click-angle5").addClass("fa-angle-up");
+            $("#click-angle5").removeClass("fa-angle-down");
+            $("#click-angle3").addClass("fa-angle-up");
+            $("#click-angle3").removeClass("fa-angle-down");
 
         }
         // console.log($("#click-angle").attr("class").split(" ")[2]);
@@ -104,7 +217,7 @@ $(document).ready(function () {
 
 $('.carousel').carousel({
     interval: 1000
-  })
+})
 
 // $(document).ready(function(){
 //     $("#carouselID").click(function(){
@@ -140,37 +253,241 @@ $('.carousel').carousel({
 
 // });
 
-    //   khai báo biến slideIndex đại diện cho slide hiện tại
-    //   var slideIndex;
-    //   // KHai bào hàm hiển thị slide
-    //   function showSlides() {
-    //       var i;
-    //       var slides = document.getElementsByClassName("mySlides");
-    //       var dots = document.getElementsByClassName("dot");
+//   khai báo biến slideIndex đại diện cho slide hiện tại
+//   var slideIndex;
+//   // KHai bào hàm hiển thị slide
+//   function showSlides() {
+//       var i;
+//       var slides = document.getElementsByClassName("mySlides");
+//       var dots = document.getElementsByClassName("dot");
 
-    //       for (i = 0; i < slides.length; i++) {
-    //          slides[i].style.display = "none";  
-    //       }
-    //       for (i = 0; i < dots.length; i++) {
-    //           dots[i].className = dots[i].className.replace(" active", "");
-    //       }
+//       for (i = 0; i < slides.length; i++) {
+//          slides[i].style.display = "none";  
+//       }
+//       for (i = 0; i < dots.length; i++) {
+//           dots[i].className = dots[i].className.replace(" active", "");
+//       }
 
-    //       slides[slideIndex].style.display = "block";  
-    //       dots[slideIndex].className += " active";
-    //       //chuyển đến slide tiếp theo
-    //       slideIndex++;
-    //       //nếu đang ở slide cuối cùng thì chuyển về slide đầu
-    //       if (slideIndex > slides.length - 1) {
-    //         slideIndex = 0
-    //       }    
-    //       //tự động chuyển đổi slide sau 5s
-    //       setTimeout(showSlides, 4000);
-    //   }
-    //   //mặc định hiển thị slide đầu tiên 
-    //   showSlides(slideIndex = 0);
+//       slides[slideIndex].style.display = "block";  
+//       dots[slideIndex].className += " active";
+//       //chuyển đến slide tiếp theo
+//       slideIndex++;
+//       //nếu đang ở slide cuối cùng thì chuyển về slide đầu
+//       if (slideIndex > slides.length - 1) {
+//         slideIndex = 0
+//       }    
+//       //tự động chuyển đổi slide sau 5s
+//       setTimeout(showSlides, 4000);
+//   }
+//   //mặc định hiển thị slide đầu tiên 
+//   showSlides(slideIndex = 0);
 
 
-    //   function currentSlide(n) {
-    //     showSlides(slideIndex = n);
-    //   }
+//   function currentSlide(n) {
+//     showSlides(slideIndex = n);
+//   }
+// ngay lúc click vào
+var time1, time2;
 
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function xem() {
+    var today = new Date();
+    var a;
+
+    var DD = today.getDate();
+    var MM = today.getMonth() + 1;
+    var YYYY = today.getFullYear();
+
+    var h = today.getHours();
+    var m = today.getMinutes();
+    DD = checkTime(DD);
+    MM = checkTime(MM);
+    YYYY = checkTime(YYYY);
+
+    h = checkTime(h);
+    m = checkTime(m);
+    a = DD + "/" + MM + "/" + YYYY + " " + h + ":" + m;
+    document.getElementById("timenow").innerHTML = a;
+
+}
+// sau click 1 time
+
+
+function khoangtime() {
+
+    alert("đây là alarm");
+
+    moment.locale('vi');
+    var a = "29/10/2019 10:00";
+    var b;
+    b = moment(a, 'DD/MM/YYYY hh:mm')
+        .startOf('hour')
+        .fromNow();
+    document.getElementById("timenow2").innerHTML = b;
+
+}
+// bài 1
+function kiemtrasonguyento() {
+    var a = document.getElementById("songuyento").value;
+    if (isNaN(a)) {
+        //cai nay la neu input nhap vao khong phai la so
+        alert("Bạn hãy nhập vào 1 chữ số")
+        return false;
+    } else {
+        //neu nhap vao la so, cho phep submit
+
+        // alert(a);
+        var tmp = true;
+        if (a < 2) {
+            tmp = false;
+        }
+        else {
+            for (var i = 2; i < a - 1; i++) {
+                if (a % i == 0) {
+                    tmp = false;
+                    break;
+                }
+            }
+        }
+        if (tmp == true) {
+            // alert("so nguyen to");
+
+            document.getElementById("result").innerHTML = "Đây là số nguyên tố";
+        }
+        else {
+            // alert("Khong phai la so nguyen to");
+            document.getElementById("result").innerHTML = "Đây không phải là số nguyên tố";
+
+        }
+    }
+}
+// bài 2
+function daonguoc() {
+    var a = document.getElementById("sodaonguoc").value;
+
+    if (isNaN(a)) {
+        //cai nay la neu input nhap vao khong phai la so
+        alert("Bạn hãy nhập vào 1 chữ số")
+        return false;
+    } else {
+        var b = a.split('').reverse().join('');
+        // reverseString(a);
+        document.getElementById("result-Daonguoc").innerHTML = "Số " + a + " sau khi đảo ngược là: " + b;
+
+    }
+
+    // var a = document.getElementById("kitulap").value;
+    // var b = a.split('').reverse().join('');
+    // var l = b.length;
+    // var tmp;
+    // for (var i = 0; i < l - 1; i++) {
+    //     for (var j = i; j < l; j++) {
+    //         if (b[i] == b[j]) {
+    //             tmp = b[i+1];
+    //             b[i+1] = b[j];
+    //             b[j] = tmp;
+    //             i++;
+    //             break;
+    //         }
+    //     }
+    // }
+    // alert(b);
+}
+// bài 3
+function kethop() {
+    var a = document.getElementById("kethop").value;
+
+    var b = a.split('').join('');
+    var dem = [];
+    var k = 0;
+    for (var i = 0; i < b.length; i++) {
+        for (var j = i + 1; j <= b.length; j++) {
+            dem[k] = b.substring(i, j);
+            k++;
+        }
+    }
+    // alert(dem);
+    document.getElementById("result-kethop").innerHTML = "Chuỗi sau khi kết hợp là " + dem;
+
+
+}
+// bài 4
+function timsolap() {
+    var a = document.getElementById("kitulap").value;
+    var b = a.split('').join('');
+    alert(b);
+    var c = Array.from(new Set(b));
+    // alert(c); xóa các phần tử trùng lặp trong mảng
+    var dem = []; //đếm số lần xuất hiện.
+    for (var i = 0; i < c.length; i++) {
+        dem[i] = b.split(c[i]).length -1;
+    }
+    document.getElementById("result-kitulap").innerHTML = "Bạn đã nhập vào chuỗi " + a + " gồm có các kí tự:  " + c + " tương ứng với số lần xuất hiện " + dem;
+}
+// bài 5
+var mang = [];
+var i=0;
+function nhapsophantucuamang() {
+  
+    mang[i] = document.getElementById("nhapmang").value;
+    i++;
+    
+    document.getElementById("mang").innerHTML = mang;
+
+}
+function xoamang(){
+    mang.splice(0, mang.length);
+    i=0;
+    document.getElementById("mang").innerHTML = mang;
+
+}
+
+
+function sosanhtrongmang() {
+    var a = document.getElementById("kitucansosanh").value;
+    var tmp = [],result =[];
+    var dem =0;
+    tmp=mang;
+    for (var i = 0; i < tmp.length; i++) {
+        if(tmp[i]>a){
+            result[dem]=tmp[i];
+            dem++;
+        }
+    }
+    document.getElementById("result-sosanhtrongmang").innerHTML = "Mảng bạn nhập vào là: " + mang + " các phần tử lớn hơn " + a+ " là :" +result;
+    
+}
+// Bài 6
+function chuoikitu() {
+    var a = document.getElementById("chuoikitu").value;
+    var b = document.getElementById("kitucantim").value;
+    var tmp = a.split('').join('');
+    var dem = a.split(b).length - 1;
+    document.getElementById("result-chuoikitu").innerHTML = " chữ " + b + " xuất hiện trong chuỗi " + a + " tới " + dem + " lần";
+
+}
+// bài 7
+
+function bai7() {
+    var a = document.getElementById("bai7").value;
+    var b = a.split('').join('');
+    var dem = [];
+    for (var i = 0; i < b.length; i++) {
+        dem[i] = b.split(b[i]).length - 1;
+    }
+    for (var i = 0; i < dem.length; i++) {
+        if (dem[i] == 1) {
+            document.getElementById("result-bai7").innerHTML = "Chữ số không bị lặp xuất hiện đầu tiên là: " + b[i];
+            return;
+        }
+    }
+
+
+
+}
